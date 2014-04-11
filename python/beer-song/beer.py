c965e0103bcf734a539af511373379b4f297325c
Bistik <kungperl@gmail.com>
2014-04-12 01:12:58 +0800

    1st submission python/beer-song

--- python/beer-song/beer.py
+++ python/beer-song/beer.py
@@ -1,39 +1,41 @@
 class Beer(object):
+
     def sing(self, *args):
         verses = []
-        start = args[0]
-        end = -1
+        (start, end) = (args[0], -1)
+
         if len(args) > 1:
             end = end + args[1]
+
         for i in range(args[0],end,-1):
             verses.append(self.verse(i))
-        return "\n".join(verses)
 
-            
+        return "\n".join(verses) + "\n"
+
+
     def verse(self, n):
-        return self._sing(n)
-
-    def _sing(self, n):
         bottle_n = self._plural(n)
         bottle_less = self._plural(n-1)
         if n >= 1:
-            verse = ("%s of beer on the wall, %s of beer.\n"
-                    "Take %s down and pass it around, %s of beer on the wall.\n" %(bottle_n, bottle_n, self._it_or_one(n), bottle_less)) 
+            return  ("%s of beer on the wall, %s of beer.\n"
+                     "Take %s down and pass it around, %s of beer on the wall.\n" %(
+                                            bottle_n,
+                                            bottle_n,
+                                            self._it_or_one(n),
+                                            bottle_less)
+                                        ) 
         else:
-            verse = ("No more bottles of beer on the wall, no more bottles of beer.\n"
-"Go to the store and buy some more, 99 bottles of beer on the wall.\n\n")
-        return verse
+            return ("No more bottles of beer on the wall, no more bottles of beer.\n"
+                    "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
 
     def _it_or_one(self,n):
         if n > 1:
             return "one"
-        else:
-            return "it"
+        return "it"
 
     def _plural(self,n):
         if n > 1:
             return "%d bottles" %(n)
-        elif n == 1:
-            return "%d bottle" %(n)
-        else:
-            return 'no more bottles'
+        if n == 1:
+            return "1 bottle"
+        return 'no more bottles'
