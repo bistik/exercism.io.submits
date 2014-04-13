c793f51e6b6d06f056f7609af577c240aff66380
Bistik <kungperl@gmail.com>
2014-04-13 16:20:03 +0800

    remove dna attribute. rename variables. 2nd submission

--- python/nucleotide-count/dna.py
+++ python/nucleotide-count/dna.py
@@ -1,19 +1,18 @@
 class DNA(object):
     def __init__(self, dna_str):
-        self.dna = dna_str
         self.nucleotides = { 'A': 0, 'C': 0, 'G': 0, 'T': 0 }
-        for e in self.dna:
-            if e in self.nucleotides:
-                self.nucleotides[e] = self.nucleotides[e] + 1
+        for nuc in dna_str:
+            if nuc in self.nucleotides:
+                self.nucleotides[nuc] += 1
             else:
-                self.nucleotides[e] = 1
+                self.nucleotides[nuc] = 1
 
-    def count(self, dna_str):
+    def count(self, nucleotide):
         nucs_plus_u = dict(self.nucleotides)
         nucs_plus_u['U'] = 0
-        if dna_str in nucs_plus_u:
-            return nucs_plus_u[dna_str]
-        raise ValueError('%s is not a nucleotide.' %(dna_str))
+        if nucleotide in nucs_plus_u:
+            return nucs_plus_u[nucleotide]
+        raise ValueError('%s is not a nucleotide.' %(nucleotide))
 
     def nucleotide_counts(self):
         return self.nucleotides
