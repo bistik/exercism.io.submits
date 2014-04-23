09bd1b2d1af316e602077cbd5fb5c7378640774d
Eman <kungperl@gmail.com>
2014-04-23 10:46:15 +0800

    4th submission python/difference_of_squares

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -2,9 +2,7 @@ def difference(n):
     return square_of_sum(n) - sum_of_squares(n)
 
 def square_of_sum(n):
-    sum_to_square = sum(xrange(1, n+1))
-    return sum_to_square ** 2
+    return sum(xrange(1, n+1)) ** 2
 
 def sum_of_squares(n):
-    squares = map(lambda x: x**2, xrange(1, n+1))
-    return sum(squares)
+    return sum(x**2 for x in xrange(1, n+1))
